11ccf56cc5274d2f60783906311c2523e17c510d
Marcin Siwonia <mahinek12@gmail.com>
2023-09-03 12:55:13 +0200

    Add scroll fix

--- ms-portfolio/src/components/pages/Projects/Projects.js
+++ ms-portfolio/src/components/pages/Projects/Projects.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import styles from "./Projects.module.scss";
 import Transition from "../../features/Transition/Transition";
 
