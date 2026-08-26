9636c609af8467ab4825b8fd7e03690067ed1579
Marcin Siwonia <mahinek12@gmail.com>
2026-08-26 08:16:21 +0200

    Higiena repo: gitignore na logi i artefakty, robots blokuje /lab
    Katalog glowny nie mial gitignore, wiec artefakt builda .next-build-ms
    i 21 logow roboczych czekalo na wpadniecie do repo. /lab to warianty
    projektowe, maja noindex, ale robots pozwalal na /, wiec dochodzil do nich
    kazdy, kto zgadl adres.

--- portfolio-redesign/app/robots.ts
+++ portfolio-redesign/app/robots.ts
@@ -4,7 +4,8 @@ const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.marcinsiwonia.
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    // /lab to warianty projektowe, nie treść dla odwiedzających.
+    rules: { userAgent: "*", allow: "/", disallow: "/lab" },
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
   };
